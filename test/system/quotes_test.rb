1f094a62762625f1c0914348a3d4b345cd8358b7
momo-87 <momochristian87@gmail.com>
2024-01-18 16:40:35 +0100

    Using fake data from quotes.yml fixtures file to create more system test

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
+  setup do
+    @quote = quotes(:first) # Reference to the first fixture quote (see test/fixtures/quotes.yml)
+  end
+
   test "Creating a new quote" do
     # when we visit the quotes Quotes#index page
     # we expect to see a title with the text "Quotes"
@@ -22,4 +26,33 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
     assert_text "Momo quote"
   end
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit quote"
+
+    fill_in "name", with: "Updated quote"
+    click_on "Update Quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end  
 end
