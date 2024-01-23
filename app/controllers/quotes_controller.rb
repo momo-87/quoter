4d70295a4159f8fdb991d0f7720ece3d94614d66
momo-87 <momochristian87@gmail.com>
2024-01-23 11:52:47 +0100

    Fix the Turbo Drive: Form responses must redirect to another location browser console error when submitting invalid data.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -27,7 +27,7 @@ class QuotesController < ApplicationController
     if @quote.save
       redirect_to quotes_path, notice: "Quote was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -42,7 +42,7 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was successfully updated."
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
