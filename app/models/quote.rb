60e4c540d3c4e7618c8c2d870fe16fdd28634c59
momo-87 <momochristian87@gmail.com>
2024-02-12 16:35:19 +0100

    Broadcasting quote updates with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,4 +6,5 @@ class Quote < ApplicationRecord
 
   # Broadcasting created quotes with Turbo Streams
   after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
 end
