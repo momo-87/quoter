b44de5f308192da1bf2d9a22bdae099703864b33
momo-87 <momochristian87@gmail.com>
2024-02-12 16:39:46 +0100

    Broadcasting quote deletion with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,4 +7,5 @@ class Quote < ApplicationRecord
   # Broadcasting created quotes with Turbo Streams
   after_create_commit -> { broadcast_prepend_to "quotes" }
   after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
