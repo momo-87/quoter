0288429ae2bbb593d6bfbe9a36a2d98b0d93a2a4
momo-87 <momochristian87@gmail.com>
2024-02-12 17:16:51 +0100

    syntactic convention to avoid writing the three callbacks all the time

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
+
 class Quote < ApplicationRecord
   validates :name, presence: true
 
@@ -5,7 +6,5 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # Broadcasting created quotes with Turbo Streams
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
