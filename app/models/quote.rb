b76f07fffe34c79dcd0acacfc06e04134691c0fe
momo-87 <momochristian87@gmail.com>
2024-02-12 16:52:38 +0100

    Making broadcasting asynchronous with ActiveJob

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,7 +5,7 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # Broadcasting created quotes with Turbo Streams
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
   after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
