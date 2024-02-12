5d3ccb2488339903366b55b8e9841a74c63530e8
momo-87 <momochristian87@gmail.com>
2024-02-12 16:30:14 +0100

    Turbo Streams conventions

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,5 +5,5 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # Broadcasting created quotes with Turbo Streams
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_to "quotes" }
 end
