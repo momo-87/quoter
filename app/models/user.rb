5cb321e2713b91446b89d41da6062be2ad7676f6
momo-87 <momochristian87@gmail.com>
2024-02-13 18:45:52 +0100

    Fix typo in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :validatable
 
-  belongs_to :compangy
+  belongs_to :company
 end
