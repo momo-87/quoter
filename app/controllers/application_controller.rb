a9d18fea89adde34786b040b6d5e741056f96116
momo-87 <momochristian87@gmail.com>
2024-02-14 16:34:59 +0100

    Implement the current_company logic into the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+
+  # Turn the current_company method into a helper to be able to use it in view files
+  helper_method :current_company
 end
