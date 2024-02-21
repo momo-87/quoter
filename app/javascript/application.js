27fb42e2e90af54166d72a509c2bfe66bcd78960
momo-87 <momochristian87@gmail.com>
2024-02-21 11:46:26 +0100

    Re-enable Turbo in the whole application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Entry point for the build script in your package.json
 import "./controllers"
-
-
-// The two following lines disable Turbo on the whole application
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+import "@hotwired/turbo-rails"
