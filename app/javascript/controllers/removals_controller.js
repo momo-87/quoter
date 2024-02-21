13b0f6180dcd6f2f37a55626213889eb0b065bc3
momo-87 <momochristian87@gmail.com>
2024-02-21 11:20:53 +0100

    Implement the Removals Stimulus controller

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -1,7 +1,9 @@
+
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="removals"
 export default class extends Controller {
-  connect() {
+  remove() {
+    this.element.remove()
   }
 }
