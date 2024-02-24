f51b9e193f51c7ddea7aae33b25b1d299fd323f3
momo-87 <momochristian87@gmail.com>
2024-02-24 11:13:48 +0100

    Add null: false constraint on date

--- db/migrate/20240223133707_create_line_item_dates.rb
+++ db/migrate/20240223133707_create_line_item_dates.rb
@@ -2,9 +2,12 @@ class CreateLineItemDates < ActiveRecord::Migration[7.0]
   def change
     create_table :line_item_dates do |t|
       t.references :quote, null: false, foreign_key: true
-      t.date :date
+
+      # Adding null: false constraint on date
+      t.date :date, null: false
 
       t.timestamps
     end
+    
   end
 end
