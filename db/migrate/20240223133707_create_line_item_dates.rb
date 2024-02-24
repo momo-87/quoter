b6f54a4c794ffbfee5867d183e9afd5cc71253ab
momo-87 <momochristian87@gmail.com>
2024-02-24 11:15:19 +0100

    Adding index to the date field for performance reasons

--- db/migrate/20240223133707_create_line_item_dates.rb
+++ db/migrate/20240223133707_create_line_item_dates.rb
@@ -9,7 +9,9 @@ class CreateLineItemDates < ActiveRecord::Migration[7.0]
       t.timestamps
     end
     
-        # Adding uniqueness constraint for the couple date and quote_id
-        add_index :line_item_dates, [:date, :quote_id], unique: true
+    # Adding uniqueness constraint for the couple date and quote_id
+    add_index :line_item_dates, [:date, :quote_id], unique: true
+    # Adding index to the date field for performance reasons
+    add_index :line_item_dates, :date
   end
 end
