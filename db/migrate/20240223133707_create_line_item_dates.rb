55527dc770a18195e3f7ab13fbc78ad80bfa560a
momo-87 <momochristian87@gmail.com>
2024-02-24 11:14:34 +0100

    Add uniqueness constraint for the couple date and quote_id

--- db/migrate/20240223133707_create_line_item_dates.rb
+++ db/migrate/20240223133707_create_line_item_dates.rb
@@ -9,5 +9,7 @@ class CreateLineItemDates < ActiveRecord::Migration[7.0]
       t.timestamps
     end
     
+        # Adding uniqueness constraint for the couple date and quote_id
+        add_index :line_item_dates, [:date, :quote_id], unique: true
   end
 end
