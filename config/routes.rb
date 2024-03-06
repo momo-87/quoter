e8b835a3d28314422cf67fbf8c05854f90f431e3
momo-87 <momochristian87@gmail.com>
2024-03-06 12:45:49 +0100

    Fix syntax error in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :quotes do
     resources :line_item_dates, except: [:index, :show] do
       resources :line_items, except: [:index, :show]
+    end
   end
 end
