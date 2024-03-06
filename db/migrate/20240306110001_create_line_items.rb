348b7acc65f0954403debecaddf4b97a14362bc7
momo-87 <momochristian87@gmail.com>
2024-03-06 12:08:24 +0100

    Enforce LineItem name, quantity and unit_price fields validation at the database level

--- db/migrate/20240306110001_create_line_items.rb
+++ db/migrate/20240306110001_create_line_items.rb
@@ -2,10 +2,10 @@ class CreateLineItems < ActiveRecord::Migration[7.0]
   def change
     create_table :line_items do |t|
       t.references :line_item_date, null: false, foreign_key: true
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.integer :quantity
-      t.decimal :unit_price, precision: 10, scale: 2
+      t.integer :quantity, null: false
+      t.decimal :unit_price, precision: 10, scale: 2, null: false
 
       t.timestamps
     end
