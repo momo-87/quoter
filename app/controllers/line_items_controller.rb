680aeeaf64981db737ca05fcf66dc8ccfad04ead
momo-87 <momochristian87@gmail.com>
2024-03-11 15:15:45 +0100

    Add the #edit and #update actions to the LineItemsController

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: [:edit, :update, :destroy]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -16,6 +17,17 @@ class LineItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      redirect_to quote_path(@quote), notice: "Item was successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def line_item_params
@@ -28,5 +40,9 @@ class LineItemsController < ApplicationController
 
   def set_line_item_date
     @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+  end
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find(params[:id])
   end
 end
